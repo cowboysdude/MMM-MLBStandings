1dd0964402da6c8cc7a796f23df6d7329fe45a59
John Wade <cowboysdude@stny.rr.com>
2017-04-02 14:16:09 -0400

    Changes

--- MMM-MLBstandings.js
+++ MMM-MLBstandings.js
@@ -12,10 +12,10 @@ Module.register("MMM-MLBstandings", {
         animationSpeed: 10,
         initialLoadDelay: 2500, // 2.5 seconds delay
         retryDelay: 1500,
-        maxWidth: "100%",
+        maxWidth: "450px",
         fadeSpeed: 5,
         header: true,
-        rotateInterval: 20 * 1000
+        rotateInterval: 10 * 1000
     },
 
     // Define required scripts.
@@ -33,25 +33,24 @@ Module.register("MMM-MLBstandings", {
         this.sendSocketNotification('CONFIG', this.config);
         // Set locale.
         this.week = "";
-        this.americanEast = {};
-        this.nationale = {};
-        this.americanCen = {};
-        this.nationalc = {};
-        this.americanWest = {};
-        this.nationalw = {};
+        this.conferences = ["AL", "NL"];
+        this.divisions = ["W", "C", "E"];
         this.today = "";
         this.activeItem = 0;
         this.updateInterval = null;
         this.scheduleUpdate();
     },
-    
+
     scheduleCarousel: function() {
-       		console.log("Scheduling History items");
-	   		this.rotateInterval = setInterval(() => {
-				this.activeItem++;
-				this.updateDom(this.config.animationSpeed);
-			}, this.config.rotateInterval);
-	   },
+        console.log("Scheduling History items");
+        this.rotateInterval = setInterval(() => {
+            this.activeItem++;
+            if (this.activeItem >= 6) {
+                this.activeItem = 0;
+            }
+            this.updateDom(this.config.animationSpeed);
+        }, this.config.rotateInterval);
+    },
 
     scheduleUpdate: function() {
         this.updateInterval = setInterval(() => {
@@ -67,20 +66,15 @@ Module.register("MMM-MLBstandings", {
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === "STANDINGS_RESULTS") {
-        	this.americanEast = payload.AmericanE;
-            this.nationale = payload.Nationale;
-            this.americanCen = payload.AmericanC;
-            this.nationalc = payload.Nationalc;
-            this.americanWest = payload.AmericanW;
-            this.nationalw = payload.Nationalw;
-    console.log(this.nationalw);
-            if(this.rotateInterval == null){
-			   	this.scheduleCarousel();
-			   }
-               this.updateDom(this.config.animationSpeed);
-         }
-         this.updateDom(this.config.initialLoadDelay);
-     },
+            console.log(payload);
+            this.standings = payload.standings
+            if (this.rotateInterval == null) {
+                this.scheduleCarousel();
+            }
+            this.updateDom(this.config.animationSpeed);
+        }
+        this.updateDom(this.config.initialLoadDelay);
+    },
 
     getDom: function() {
 
@@ -99,283 +93,127 @@ Module.register("MMM-MLBstandings", {
             wrapper.appendChild(header);
         }
 
-           
+        var top = document.createElement("div");
+        top.classList = "xsmall bright thin";
 
-            var top = document.createElement("div");
-            top.classList = "xsmall bright thin";
+        // Table creation below
 
-            // Table creation below
+        var gameTable = document.createElement("table");
 
-            var gameTable = document.createElement("table");
+        var firstrow = document.createElement("tr");
 
-            var firstrow = document.createElement("tr");
+        var teamcolumn = document.createElement("th");
+        teamcolumn.setAttribute("colspan", 2);
+        teamcolumn.classList.add("status");
+        teamcolumn.innerHTML = "Team";
 
-            var teamcolumn = document.createElement("th");
-            teamcolumn.setAttribute("colspan", 4);
-            teamcolumn.classList.add("status");
-            teamcolumn.innerHTML = "Team";
+        firstrow.appendChild(teamcolumn);
+        gameTable.appendChild(firstrow);
 
-            firstrow.appendChild(teamcolumn);
-            gameTable.appendChild(firstrow);
+        var winscolumn = document.createElement("th");
+        winscolumn.classList.add("r");
+        winscolumn.innerHTML = "W";
+        firstrow.appendChild(winscolumn);
+        gameTable.appendChild(firstrow);
 
-            var winscolumn = document.createElement("th");
-            winscolumn.setAttribute("colspan", 4);
-            winscolumn.classList.add("r");
-            winscolumn.innerHTML = "W";
-            firstrow.appendChild(winscolumn);
-            gameTable.appendChild(firstrow);
+        var losscolumn = document.createElement("th");
+        losscolumn.classList.add("h");
+        losscolumn.innerHTML = "L";
+        firstrow.appendChild(losscolumn);
+        gameTable.appendChild(firstrow);
 
-            var losscolumn = document.createElement("th");
-            losscolumn.setAttribute("colspan", 4);
-            losscolumn.classList.add("h");
-            losscolumn.innerHTML = "L";
-            firstrow.appendChild(losscolumn);
-            gameTable.appendChild(firstrow);
+        var gbcolumn = document.createElement("th");
+        gbcolumn.classList.add("h");
+        gbcolumn.innerHTML = "GB";
+        firstrow.appendChild(gbcolumn);
+        gameTable.appendChild(firstrow);
 
-            var gbcolumn = document.createElement("th");
-            gbcolumn.setAttribute("colspan", 4);
-            gbcolumn.classList.add("h");
-            gbcolumn.innerHTML = "GB";
-            firstrow.appendChild(gbcolumn);
-            gameTable.appendChild(firstrow);
+        var wpcolumn = document.createElement("th");
+        wpcolumn.classList.add("h");
+        wpcolumn.innerHTML = "W%";
+        firstrow.appendChild(wpcolumn);
+        gameTable.appendChild(firstrow);
 
-            var wpcolumn = document.createElement("th");
-            wpcolumn.setAttribute("colspan", 4);
-            wpcolumn.classList.add("h");
-            wpcolumn.innerHTML = "W%";
-            firstrow.appendChild(wpcolumn);
-            gameTable.appendChild(firstrow);
+        var gpcolumn = document.createElement("th");
+        gpcolumn.classList.add("h");
+        gpcolumn.innerHTML = "GP";
+        firstrow.appendChild(gpcolumn);
 
-            var gpcolumn = document.createElement("th");
-            gpcolumn.setAttribute("colspan", 4);
-            gpcolumn.classList.add("h");
-            gpcolumn.innerHTML = "GP";
-            firstrow.appendChild(gpcolumn);
+        var tencolumn = document.createElement("th");
+        tencolumn.classList.add("h");
+        tencolumn.innerHTML = "L10";
+        firstrow.appendChild(tencolumn);
 
-            var tencolumn = document.createElement("th");
-            tencolumn.setAttribute("colspan", 4);
-            tencolumn.classList.add("h");
-            tencolumn.innerHTML = "L10";
-            firstrow.appendChild(tencolumn);
-
-            var streakcolumn = document.createElement("th");
-            streakcolumn.setAttribute("colspan", 4);
-            streakcolumn.classList.add("h");
-            streakcolumn.innerHTML = "Streak";
-            firstrow.appendChild(streakcolumn);
+        var streakcolumn = document.createElement("th");
+        streakcolumn.classList.add("h");
+        streakcolumn.innerHTML = "Streak";
+        firstrow.appendChild(streakcolumn);
 
 
-            gameTable.appendChild(firstrow);
+        gameTable.appendChild(firstrow);
 
 
-            //-----------------------------------//
-            //         Data goes here            //
-            //-----------------------------------//
-            
-            for (var i = 0; i < this.americanCen.length; i++) {
-                var standings = this.americanCen[i];
-           
+        //-----------------------------------//
+        //         Data goes here            //
+        //-----------------------------------//
+        var conference = this.conferences[this.activeItem < 3 ? 0 : 1];
+        var division = this.divisions[this.activeItem % 3];
+        var standings = this.standings[conference][division];
 
-                var standingTemp = document.createElement("tr");
-
-                  if (standings.division === "E") {
-            
-
-                    var standingTempColumn = document.createElement("td");
-            var teamImg = '<img class="logo" src="modules/MMM-MLBstandings/icons/' + standings.last_name + '.png"> ' + standings.first_name + " " + standings.last_name;
-                    var teamImg = teamImg;
-                    standingTempColumn.setAttribute("colspan", 4);
-                    standingTempColumn.classList.add("rows");
-                    standingTempColumn.innerHTML = teamImg;
-                    standingTemp.appendChild(standingTempColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var winsColumn = document.createElement("td");
-                    winsColumn.setAttribute("colspan", 4);
-                    winsColumn.innerHTML = standings.won;
-                    standingTemp.appendChild(winsColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var lossColumn = document.createElement("td");
-                    lossColumn.setAttribute("colspan", 4);
-                    lossColumn.innerHTML = standings.lost;
-                    standingTemp.appendChild(lossColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var gbColumn = document.createElement("td");
-                    gbColumn.setAttribute("colspan", 4);
-                    gbColumn.innerHTML = standings.games_back;
-                    standingTemp.appendChild(gbColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var wpColumn = document.createElement("td");
-                    wpColumn.setAttribute("colspan", 4);
-                    wpColumn.innerHTML = standings.win_percentage;
-                    standingTemp.appendChild(wpColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var gpColumn = document.createElement("td");
-                    gpColumn.setAttribute("colspan", 4);
-                    gpColumn.innerHTML = standings.games_played;
-                    standingTemp.appendChild(gpColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var tenColumn = document.createElement("td");
-                    tenColumn.setAttribute("colspan", 4);
-                    tenColumn.innerHTML = standings.last_ten;
-                    standingTemp.appendChild(tenColumn);
-                    gameTable.appendChild(standingTemp);
-
-                    var streakColumn = document.createElement("td");
-                    streakColumn.setAttribute("colspan", 4);
-                    streakColumn.innerHTML = standings.streak;
-                    standingTemp.appendChild(streakColumn);
-                    gameTable.appendChild(standingTemp);
-                    
-                    var emptyRow = document.createElement("tr");
-                    var emptyColumn = document.createElement("td");
-                    emptyRow.setAttribute("colspan", 4);
-                    emptyRow.innerHTML = "";
-                    standingTemp.appendChild(emptyColumn);
-                    gameTable.appendChild(standingTemp);
-           //     }
-             }
-            if (standings.division === "C") {
-     
-             var standing2Temp = document.createElement("tr");
-
-                    var standing2TempColumn = document.createElement("td");
-            var teamImg = '<img class="logo" src="modules/MMM-MLBstandings/icons/' + standings.last_name + '.png"> ' + standings.first_name + " " + standings.last_name;
-                    var teamImg = teamImg;
-                    standing2TempColumn.setAttribute("colspan", 4);
-                    standing2TempColumn.classList.add("rows");
-                    standing2TempColumn.innerHTML = teamImg;
-                    standing2Temp.appendChild(standing2TempColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var winsColumn = document.createElement("td");
-                    winsColumn.setAttribute("colspan", 4);
-                    winsColumn.innerHTML = standings.won;
-                    standing2Temp.appendChild(winsColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var lossColumn = document.createElement("td");
-                    lossColumn.setAttribute("colspan", 4);
-                    lossColumn.innerHTML = standings.lost;
-                    standing2Temp.appendChild(lossColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var gbColumn = document.createElement("td");
-                    gbColumn.setAttribute("colspan", 4);
-                    gbColumn.innerHTML = standings.games_back;
-                    standing2Temp.appendChild(gbColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var wpColumn = document.createElement("td");
-                    wpColumn.setAttribute("colspan", 4);
-                    wpColumn.innerHTML = standings.win_percentage;
-                    standing2Temp.appendChild(wpColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var gpColumn = document.createElement("td");
-                    gpColumn.setAttribute("colspan", 4);
-                    gpColumn.innerHTML = standings.games_played;
-                    standing2Temp.appendChild(gpColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var tenColumn = document.createElement("td");
-                    tenColumn.setAttribute("colspan", 4);
-                    tenColumn.innerHTML = standings.last_ten;
-                    standing2Temp.appendChild(tenColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var streakColumn = document.createElement("td");
-                    streakColumn.setAttribute("colspan", 4);
-                    streakColumn.innerHTML = standings.streak;
-                    standing2Temp.appendChild(streakColumn);
-                    gameTable.appendChild(standing2Temp);
-                    
-                    var emptyRow = document.createElement("tr");
-                    var emptyColumn = document.createElement("td");
-                    emptyRow.setAttribute("colspan", 4);
-                    emptyRow.innerHTML = "";
-                    standing2Temp.appendChild(emptyColumn);
-                    gameTable.appendChild(standing2Temp);
-           //     }
-             }
-             
-              if (standings.division === "W") {
-     
-             var standing2Temp = document.createElement("tr");
-
-                 
-
-                    var standing2TempColumn = document.createElement("td");
-            var teamImg = '<img class="logo" src="modules/MMM-MLBstandings/icons/' + standings.last_name + '.png"> ' + standings.first_name + " " + standings.last_name;
-                    var teamImg = teamImg;
-                    standing2TempColumn.setAttribute("colspan", 4);
-                    standing2TempColumn.classList.add("rows");
-                    standing2TempColumn.innerHTML = teamImg;
-                    standing2Temp.appendChild(standing2TempColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var winsColumn = document.createElement("td");
-                    winsColumn.setAttribute("colspan", 4);
-                    winsColumn.innerHTML = standings.won;
-                    standing2Temp.appendChild(winsColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var lossColumn = document.createElement("td");
-                    lossColumn.setAttribute("colspan", 4);
-                    lossColumn.innerHTML = standings.lost;
-                    standing2Temp.appendChild(lossColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var gbColumn = document.createElement("td");
-                    gbColumn.setAttribute("colspan", 4);
-                    gbColumn.innerHTML = standings.games_back;
-                    standing2Temp.appendChild(gbColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var wpColumn = document.createElement("td");
-                    wpColumn.setAttribute("colspan", 4);
-                    wpColumn.innerHTML = standings.win_percentage;
-                    standing2Temp.appendChild(wpColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var gpColumn = document.createElement("td");
-                    gpColumn.setAttribute("colspan", 4);
-                    gpColumn.innerHTML = standings.games_played;
-                    standing2Temp.appendChild(gpColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var tenColumn = document.createElement("td");
-                    tenColumn.setAttribute("colspan", 4);
-                    tenColumn.innerHTML = standings.last_ten;
-                    standing2Temp.appendChild(tenColumn);
-                    gameTable.appendChild(standing2Temp);
-
-                    var streakColumn = document.createElement("td");
-                    streakColumn.setAttribute("colspan", 4);
-                    streakColumn.innerHTML = standings.streak;
-                    standing2Temp.appendChild(streakColumn);
-                    gameTable.appendChild(standing2Temp);
-                    
-                    var emptyRow = document.createElement("tr");
-                    var emptyColumn = document.createElement("td");
-                    emptyRow.setAttribute("colspan", 4);
-                    emptyRow.innerHTML = "";
-                    standing2Temp.appendChild(emptyColumn);
-                    gameTable.appendChild(standing2Temp);
-           //     }
-             }
-             
-            top.appendChild(gameTable);
-            wrapper.appendChild(top);
+        for (var i = 0; i < standings.length; i++) {
+            gameTable.appendChild(this.createStandingDataRow(standings[i]));
         }
-     
-     
-     
+
+        top.appendChild(gameTable);
+        wrapper.appendChild(top);
+
+
+
         return wrapper;
     },
+
+    createStandingDataRow: function(data) {
+        var row = document.createElement("tr");
+
+        var iconColumn = document.createElement("td");
+        var icon = document.createElement("img");
+        icon.classList.add("logo");
+        icon.src = "modules/MMM-MLBstandings/icons/" + data.last_name + ".png";
+        iconColumn.appendChild(icon);
+        row.appendChild(iconColumn);
+
+        var teamColumn = document.createElement("td");
+        teamColumn.innerHTML = data.first_name + " " + data.last_name;
+        row.appendChild(teamColumn);
+
+        var winsColumn = document.createElement("td");
+        winsColumn.innerHTML = data.won;
+        row.appendChild(winsColumn);
+
+        var lossColumn = document.createElement("td");
+        lossColumn.innerHTML = data.lost;
+        row.appendChild(lossColumn);
+
+        var gbColumn = document.createElement("td");
+        gbColumn.innerHTML = data.games_back;
+        row.appendChild(gbColumn);
+
+        var wpColumn = document.createElement("td");
+        wpColumn.innerHTML = data.win_percentage;
+        row.appendChild(wpColumn);
+
+        var gpColumn = document.createElement("td");
+        gpColumn.innerHTML = data.games_played;
+        row.appendChild(gpColumn);
+
+        var tenColumn = document.createElement("td");
+        tenColumn.innerHTML = data.last_ten;
+        row.appendChild(tenColumn);
+
+        var streakColumn = document.createElement("td");
+        streakColumn.innerHTML = data.streak;
+        row.appendChild(streakColumn);
+
+        return row;
+    }
 });
