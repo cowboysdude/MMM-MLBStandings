9d31b733c55231df8f7cd32eb596e2f0d2db9d5a
John Wade <cowboysdude@stny.rr.com>
2017-04-04 21:17:21 -0400

    Update

--- node_helper.js
+++ node_helper.js
@@ -9,11 +9,11 @@ const request = require('request');
 
 
 module.exports = NodeHelper.create({
-
+        
     start: function() {
         console.log("Starting module: " + this.name);
     },
-
+    
     getStandings: function(url) {
         request({
             url: "https://erikberg.com/mlb/standings.json",
@@ -23,26 +23,23 @@ module.exports = NodeHelper.create({
             }
         }, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                var data = {};
+            	var data = {};
                 var result = JSON.parse(body).standing;
                 data.AL = this.filterStandings(result, "AL");
                 data.NL = this.filterStandings(result, "NL");
-
-                this.sendSocketNotification("STANDINGS_RESULTS", {
-                    standings: data
-                });
-            }
+				this.sendSocketNotification("STANDINGS_RESULTS", {standings: data});
+			}
         });
     },
-
+    
     filterStandings: function(standings, conference) {
-        const filtered = standings.filter((result) => result.conference === conference);
-        return {
-            E: filtered.filter((result) => result.division === "E"),
-            C: filtered.filter((result) => result.division === "C"),
-            W: filtered.filter((result) => result.division === "W")
-        };
-    },
+		const filtered = standings.filter((result) => result.conference === conference);
+		return {
+			E: filtered.filter((result) => result.division === "E" ),
+			C: filtered.filter((result) => result.division === "C" ),
+			W: filtered.filter((result) => result.division === "W" )
+		};
+	},
 
     socketNotificationReceived: function(notification, payload) {
         if (notification === "CONFIG") {
